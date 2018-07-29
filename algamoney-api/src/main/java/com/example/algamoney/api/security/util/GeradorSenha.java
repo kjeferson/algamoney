86fbae16371e156f5b6cff67e7c8bf30d33d8793
kjeferson <kjeferson@hotmail.com>
2018-07-29 17:57:45 -0300

    Movendo o usuário para o banco de dados

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/security/util/GeradorSenha.java
@@ -0,0 +1,14 @@
+package com.example.algamoney.api.security.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+/**
+ * Exemplo de como encodar uma senha com BCryptPasswordEncoder
+ * @author Jeferson
+ *
+ */
+public class GeradorSenha {
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("admin"));
+	}
+}

470bdf9014572554942230ba4ed056315e0cf36e
kjeferson <kjeferson@hotmail.com>
2018-07-30 21:48:59 -0300

    Implementando o logout

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/TokenResource.java
@@ -0,0 +1,34 @@
+package com.example.algamoney.api.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Classe para remover o cookie do refresh token, para realizar o logout
+ * @author Jeferson
+ *
+ */
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+	//revoke: revogar
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false); // TODO: Em producao sera true
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+	
+}
